261d0e3d7a8967d9edf52ca14932b4ebfa238f10
TheBoringBot <richardsgareth@icloud.com>
2022-05-10 15:23:19 +0200

    feat: categories links basic structure

---
+++ resources/js/Pages/Backend/Components/CategoriesList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex items-center flex-wrap w-full space-x-5">
+        <Link
+            class="rounded bg-black text-white px-2"
+            v-for="category in categories"
+            v-text="category.name"
+        />
+    </div>
+</template>
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+    components: {
+        Link,
+    },
+    props: ["categories"],
+};
+</script>
