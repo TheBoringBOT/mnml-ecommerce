193e85f8145a0e6c49e6584fe59c9b32dfa9eea5
TheBoringBot <richardsgareth@icloud.com>
2022-05-10 11:38:53 +0200

    refactor: reverted to default breeze dashboard

--- resources/js/Pages/Backend/Dashboard.vue
+++ resources/js/Pages/Backend/Dashboard.vue
@@ -1,10 +1,10 @@
 <script setup>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import {Head} from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Dashboard"/>
 
     <BreezeAuthenticatedLayout>
         <template #header>
