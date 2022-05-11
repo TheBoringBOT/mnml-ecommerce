3209863344aa177e819ca124ef951bf52679a300
TheBoringBot <richardsgareth@icloud.com>
2022-05-11 18:08:07 +0200

    feat: Routes for backend crud read data pages

--- routes/web.php
+++ routes/web.php
@@ -53,19 +53,62 @@ Route::get( '/dashboard/products', [ \App\Http\Controllers\ProductController::cl
 ] )->name( 'dashboard.products' );
 
 // create product
-Route::get( '/dashboard/product/create', function () {
-	return Inertia::render( 'Backend/Product/CreateProduct' );
-} )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.products.create' );
+Route::get( '/dashboard/product/create', [
+	\App\Http\Controllers\ProductController::class,
+	'create'
+] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.products.create' );
+
 //edit product
 Route::get( '/dashboard/product/edit/{slug}', [
 	\App\Http\Controllers\ProductController::class,
 	'edit'
 ] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.products.edit' );
 
+//delete product
+Route::get( '/dashboard/product/delete/{slug}', [
+	\App\Http\Controllers\ProductController::class,
+	'destroy'
+] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.products.delete' );
+
 // orders
 
+// show all orders
 Route::get( '/dashboard/orders', [
 	\App\Http\Controllers\OrderController::class,
 	'index'
 ] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.orders' );
 require __DIR__ . '/auth.php';
+
+// show single order
+Route::get( '/dashboard/order/{id}', [
+	\App\Http\Controllers\OrderController::class,
+	'show'
+] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.order.show' );
+require __DIR__ . '/auth.php';
+
+// update order status
+Route::get( '/dashboard/order/update/{id}', [
+	\App\Http\Controllers\OrderController::class,
+	'updateOrderStatus'
+] )->middleware( [
+	'auth',
+	'verified'
+] )->name( 'dashboard.order.update' );
+
+//== Customers Backend
+
+// show all customers
+Route::get( '/dashboard/customers', [
+	\App\Http\Controllers\CustomerController::class,
+	'index'
+] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.customers' );
+require __DIR__ . '/auth.php';
+
+// show single customer
+Route::get( '/dashboard/customer/{id}', [
+	\App\Http\Controllers\CustomerController::class,
+	'show'
+] )->middleware( [ 'auth', 'verified' ] )->name( 'dashboard.customer.show' );
+require __DIR__ . '/auth.php';
+
+
