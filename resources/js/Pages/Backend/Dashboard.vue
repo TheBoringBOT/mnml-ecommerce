33b60cd6e74b795251bc20eda9620c018a398325
TheBoringBot <richardsgareth@icloud.com>
2022-05-11 18:21:01 +0200

    feat: dashboard initial controller setup

--- resources/js/Pages/Backend/Dashboard.vue
+++ resources/js/Pages/Backend/Dashboard.vue
@@ -1,8 +1,3 @@
-<script setup>
-import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
-import {Head} from "@inertiajs/inertia-vue3";
-</script>
-
 <template>
     <Head title="Dashboard"/>
 
@@ -17,10 +12,25 @@ import {Head} from "@inertiajs/inertia-vue3";
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
+                        {{ ordersSent }}
+                        {{ ordersNotSent }}
+                        {{ customers }}
                     </div>
                 </div>
             </div>
         </div>
     </BreezeAuthenticatedLayout>
 </template>
+
+<script>
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import {Head} from "@inertiajs/inertia-vue3";
+
+export default {
+    props: ["ordersSent", "ordersNotSent", "customers"],
+    components: {
+        BreezeAuthenticatedLayout,
+        Head,
+    },
+};
+</script>
