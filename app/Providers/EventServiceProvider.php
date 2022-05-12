394d4d3cadff4acbe630bb8dc9d40094c01efeb5
TheBoringBot <richardsgareth@icloud.com>
2022-05-12 12:23:47 +0200

    feat: registered new event listeners for orders

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,31 +2,37 @@
 
 namespace App\Providers;
 
+
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Events\NewOrder;
+use App\Listeners\sendNewOrderNotification;
 
-class EventServiceProvider extends ServiceProvider
-{
-    /**
-     * The event listener mappings for the application.
-     *
-     * @var array<class-string, array<int, class-string>>
-     */
-    protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
-    ];
 
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+class EventServiceProvider extends ServiceProvider {
+	/**
+	 * The event listener mappings for the application.
+	 *
+	 * @var array<class-string, array<int, class-string>>
+	 */
+	protected $listen = [
+		Registered::class => [
+			SendEmailVerificationNotification::class,
+
+		],
+		NewOrder::class   => [
+			sendNewOrderNotification::class,
+		],
+	];
+
+	/**
+	 * Register any events for your application.
+	 *
+	 * @return void
+	 */
+	public function boot() {
+		//
+	}
 }
