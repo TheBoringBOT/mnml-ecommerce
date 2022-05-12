dbb938acb0114b1a941eb19d3a397495392cf9f8
TheBoringBot <richardsgareth@icloud.com>
2022-05-12 12:25:05 +0200

    feat: roles and permissions created for user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable {
-	use HasApiTokens, HasFactory, Notifiable, Billable;
+	use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
 
 	protected $guarded = [];
