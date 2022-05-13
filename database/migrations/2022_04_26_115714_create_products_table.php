d3811bb2425a9676f3f3a8bdbf578cad8f31ed67
TheBoringBot <richardsgareth@icloud.com>
2022-05-13 11:38:46 +0200

    feat: more product columns added to migration

--- database/migrations/2022_04_26_115714_create_products_table.php
+++ database/migrations/2022_04_26_115714_create_products_table.php
@@ -4,20 +4,33 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 class CreateProductsTable extends Migration {
+
+
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
 	public function up() {
+
+
 		Schema::create( 'products', function ( Blueprint $table ) {
+
+			// default text for demo app
+			$loremIpsum = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis. Maecenas ut tellus mi. Proin tincidunt, lectus eu volutpat mattis, ante metus lacinia tellus, vitae condimentum nulla enim bibendum nibh. Praesent turpis risus, interdum nec venenatis id, pretium sit amet purus. Interdum et malesuada fames.";
+
+			$care = "Hand wash only";
+
 			$table->id();
 			$table->string( 'name' );
-			$table->string( 'brand' );
 			$table->string( 'slug' )->unique();
 			$table->string( 'sku' )->unique();
-			$table->text( 'description' );
+			$table->text( 'description' )->default( $loremIpsum );
+			$table->text( 'story' )->default( $loremIpsum );
+			$table->string( 'care' )->default( $care );
+			$table->string( 'materials' )->default( 'Metal, Plastic' );
 			$table->integer( 'available' )->default( 0 );
 			$table->integer( 'price' )->default( 0 );
 			$table->string( 'image_url' )->nullable();
