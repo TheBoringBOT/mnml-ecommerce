308aa2d1952b8a3210a3d9888de50f8543f00fe5
TheBoringBot <richardsgareth@icloud.com>
2022-05-13 13:07:16 +0200

    feat: home page initial build

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <GuestLayout> </GuestLayout>
+</template>
+<script>
+import GuestLayout from "@/Layouts/Customer";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import { useStore } from "vuex";
+
+export default {
+    components: { Head, Link, GuestLayout },
+    // props: ["products"],
+    methods: {
+        formatCurrency(amount) {
+            amount = amount / 100;
+            return amount.toLocaleString("en-US", {
+                style: "currency",
+                currency: "USD",
+            });
+        },
+    },
+    computed: {
+        products() {
+            console.log(useStore().state);
+            return useStore().state.products;
+        },
+    },
+};
+</script>
