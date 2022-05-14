2e311eac7ce28da36d249e2537ae967a6452e5ba
TheBoringBot <richardsgareth@icloud.com>
2022-05-14 10:45:47 +0200

    feat: customer dedicated layout

--- resources/js/Layouts/Customer.vue
+++ resources/js/Layouts/Customer.vue
@@ -1,7 +1,17 @@
+<template>
+    <div class="min-h-screen flex pt-6 sm:pt-0 w-full mx-auto text-black">
+        <CustomerNav/>
+
+        <div class="py-24 w-full">
+            <slot/>
+        </div>
+    </div>
+</template>
+
 <script>
 import BreezeApplicationLogo from "@/Components/ApplicationLogo.vue";
 import CustomerNav from "@/Components/Navigation/CustomerNav";
-import { Link } from "@inertiajs/inertia-vue3";
+import {Link} from "@inertiajs/inertia-vue3";
 import CartNotification from "@/components/Checkout/CartNotification";
 
 export default {
@@ -13,14 +23,3 @@ export default {
     },
 };
 </script>
-
-<template>
-    <div class="min-h-screen flex pt-6 sm:pt-0 w-full max-w-7xl mx-auto">
-
-        <CustomerNav />
-
-        <div class="mt-6 px-6 py-4 w-full">
-            <slot />
-        </div>
-    </div>
-</template>
