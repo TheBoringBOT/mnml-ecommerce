ec7364a99999229aafc789677b79b4b6c8dfa835
TheBoringBot <richardsgareth@icloud.com>
2022-05-14 10:44:58 +0200

    feat: new 3 section nav

--- resources/js/components/Navigation/CustomerNav.vue
+++ resources/js/components/Navigation/CustomerNav.vue
@@ -1,45 +1,48 @@
 <template>
     <nav
-        class="fixed top-0 left-0 right-0 w-full z-10 bg-nav-bg text-primary-clr border-b border-secondary-bg"
+            class="fixed top-0 left-0 right-0 w-full z-10 bg-white text-primary-clr border-b border-gray-100"
     >
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div
-                class="flex justify-between md:grid md:grid-cols-3 md:grid-rows-1 h-16"
+                    class="flex justify-between md:grid md:grid-cols-3 md:grid-rows-1 h-16"
             >
-                <div class="flex justify-start">
-                    <!-- Logo -->
-                    <div class="shrink-0 flex items-center col-end-auto">
-                        <Link :href="route('home')">
-                            <BreezeApplicationLogo class="block h-9 w-auto" />
-                        </Link>
-                    </div>
-                </div>
+                <!-- left -->
+
                 <div
-                    class="hidden md:flex items-center justify-center space-x-5 col-end-auto"
+                        class="hidden md:flex items-center justify-start space-x-5 col-end-auto"
                 >
                     <BreezeNavLink
-                        :href="route('products')"
-                        :active="route().current('products')"
+                            :href="route('products')"
+                            :active="route().current('products')"
                     >
                         Shop
                     </BreezeNavLink>
                     <BreezeNavLink
-                        :href="route('about')"
-                        :active="route().current('about')"
+                            :href="route('about')"
+                            :active="route().current('about')"
                     >
                         About
                     </BreezeNavLink>
                     <BreezeNavLink
-                        :href="route('about')"
-                        :active="route().current('about')"
+                            :href="route('about')"
+                            :active="route().current('about')"
                     >
                         Contact
                     </BreezeNavLink>
                 </div>
+                <!-- Logo -->
+                <div class="flex justify-center">
+                    <div class="shrink-0 flex items-center col-end-auto">
+                        <Link :href="route('home')">
+                            <BreezeApplicationLogo class="block h-9 w-auto"/>
+                        </Link>
+                    </div>
+                </div>
 
+                <!-- right -->
                 <div
-                    class="hidden md:flex md:items-center md:ml-6 items-center justify-end space-x-5 col-end-auto"
+                        class="hidden md:flex md:items-center md:ml-6 items-center justify-end space-x-5 col-end-auto"
                 >
                     <!--<BreezeNavLink-->
                     <!--:href="route('home')"-->
@@ -49,29 +52,29 @@
                     <!--</BreezeNavLink>-->
 
                     <BreezeNavLink
-                        class="fill-black"
-                        :href="route('login')"
-                        :active="route().current('login')"
+                            class="fill-black-2 hover:fill-black transition-all"
+                            :href="route('login')"
+                            :active="route().current('login')"
                     >
                         <svg width="18" height="20" viewBox="0 0 29.06 29.06">
                             <path
-                                d="M5.55,15.91a7.36,7.36,0,0,1,0-10.39,7.28,7.28,0,0,1,5.19-2.13,7.4,7.4,0,0,1,7.37,7.37,7.29,7.29,0,0,1-7.33,7.33,7.35,7.35,0,0,1-5.2-2.17M29.06,26.67,19.43,17a10.59,10.59,0,0,0,2-6.29,10.7,10.7,0,1,0-10.7,10.7,10.59,10.59,0,0,0,6.28-2l9.63,9.63Z"
+                                    d="M5.55,15.91a7.36,7.36,0,0,1,0-10.39,7.28,7.28,0,0,1,5.19-2.13,7.4,7.4,0,0,1,7.37,7.37,7.29,7.29,0,0,1-7.33,7.33,7.35,7.35,0,0,1-5.2-2.17M29.06,26.67,19.43,17a10.59,10.59,0,0,0,2-6.29,10.7,10.7,0,1,0-10.7,10.7,10.59,10.59,0,0,0,6.28-2l9.63,9.63Z"
                             ></path>
                         </svg>
                     </BreezeNavLink>
                     <BreezeNavLink
-                        class="relative fill-black"
-                        :href="route('register')"
-                        :active="route().current('register')"
+                            class="relative fill-black-2 hover:fill-black transition-all"
+                            :href="route('register')"
+                            :active="route().current('register')"
                     >
                         <span
-                            class="absolute -right-[5px] top-0 bg-black text-white rounded-full h-[17px] w-[17px] font-bold text-monospace flex items-center justify-center text-[11px]"
-                            >3</span
+                                class="absolute -right-[5px] top-0 bg-brand text-white rounded-full h-[17px] w-[17px] font-bold text-monospace flex items-center justify-center text-[11px]"
+                        >3</span
                         >
                         <figure>
                             <svg width="18" height="20" viewBox="0 0 35 48.03">
                                 <path
-                                    d="M31.62,44.65H3.38V16.3h3.4V23h3.38V16.3H24.84V23h3.38V16.3h3.4ZM10.16,10.72a7.34,7.34,0,1,1,14.68,0v2.2H10.16Zm18.06,2.2v-2.2a10.72,10.72,0,1,0-21.43,0v2.2H0V48H35V12.92Z"
+                                        d="M31.62,44.65H3.38V16.3h3.4V23h3.38V16.3H24.84V23h3.38V16.3h3.4ZM10.16,10.72a7.34,7.34,0,1,1,14.68,0v2.2H10.16Zm18.06,2.2v-2.2a10.72,10.72,0,1,0-21.43,0v2.2H0V48H35V12.92Z"
                                 ></path>
                             </svg>
                         </figure>
@@ -81,36 +84,36 @@
                 <!-- Hamburger -->
                 <div class="-mr-2 flex items-center md:hidden">
                     <button
-                        @click="
+                            @click="
                             showingNavigationDropdown = !showingNavigationDropdown
                         "
-                        class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
                     >
                         <svg
-                            class="h-6 w-6"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 24 24"
+                                class="h-6 w-6"
+                                stroke="currentColor"
+                                fill="none"
+                                viewBox="0 0 24 24"
                         >
                             <path
-                                :class="{
+                                    :class="{
                                     hidden: showingNavigationDropdown,
                                     'inline-flex': !showingNavigationDropdown,
                                 }"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M4 6h16M4 12h16M4 18h16"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
                             />
                             <path
-                                :class="{
+                                    :class="{
                                     hidden: !showingNavigationDropdown,
                                     'inline-flex': showingNavigationDropdown,
                                 }"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M6 18L18 6M6 6l12 12"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
                             />
                         </svg>
                     </button>
@@ -120,40 +123,40 @@
 
         <!-- Responsive Navigation Menu -->
         <div
-            :class="{
+                :class="{
                 block: showingNavigationDropdown,
                 hidden: !showingNavigationDropdown,
             }"
-            class="md:hidden pl-3 border-t-2 border-secondary-bg"
+                class="md:hidden pl-3 border-t-2 border-secondary-bg"
         >
             <div class="pt-2 pb-3 space-y-1">
                 <BreezeResponsiveNavLink
-                    :href="route('home')"
-                    :active="route().current('home')"
+                        :href="route('home')"
+                        :active="route().current('home')"
                 >
                     Home
                 </BreezeResponsiveNavLink>
             </div>
             <div class="pt-2 pb-3 space-y-1">
                 <BreezeResponsiveNavLink
-                    :href="route('products')"
-                    :active="route().current('products')"
+                        :href="route('products')"
+                        :active="route().current('products')"
                 >
                     Products
                 </BreezeResponsiveNavLink>
             </div>
             <div class="pt-2 pb-3 space-y-1">
                 <BreezeResponsiveNavLink
-                    :href="route('about')"
-                    :active="route().current('about')"
+                        :href="route('about')"
+                        :active="route().current('about')"
                 >
                     About
                 </BreezeResponsiveNavLink>
             </div>
             <div class="pt-2 pb-3 space-y-1">
                 <BreezeResponsiveNavLink
-                    :href="route('search')"
-                    :active="route().current('search')"
+                        :href="route('search')"
+                        :active="route().current('search')"
                 >
                     Cart
                 </BreezeResponsiveNavLink>
@@ -161,8 +164,8 @@
 
             <div class="pt-2 pb-3 space-y-1">
                 <BreezeResponsiveNavLink
-                    :href="route('order.checkout')"
-                    :active="route().current('order.checkout')"
+                        :href="route('order.checkout')"
+                        :active="route().current('order.checkout')"
                 >
                     cart
                 </BreezeResponsiveNavLink>
@@ -177,7 +180,7 @@ import BreezeDropdown from "@/Components/Dropdown.vue";
 import BreezeDropdownLink from "@/Components/DropdownLink.vue";
 import BreezeNavLink from "@/Components/NavLink.vue";
 import BreezeResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
-import { Link } from "@inertiajs/inertia-vue3";
+import {Link} from "@inertiajs/inertia-vue3";
 
 export default {
     components: {
