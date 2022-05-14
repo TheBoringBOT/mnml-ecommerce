3972298379163973e80dbd9d08087d9bb541c935
TheBoringBot <richardsgareth@icloud.com>
2022-05-14 10:39:27 +0200

    feat: social sharing component

---
+++ resources/js/components/SocialSharing/Facebook.vue
@@ -0,0 +1,30 @@
+<template>
+    <ShareNetwork
+            network="facebook"
+            :url="shareInfo.url"
+            :title="shareInfo.title"
+            :description="shareInfo.description"
+    >
+        <svg
+                width="16"
+                height="16"
+                id="Layer_1"
+                data-name="Layer 1"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 10 10"
+        >
+            <defs></defs>
+            <title>Facebook icon</title>
+            <path
+                    fill="currentColor"
+                    d="M7.64,4.73H6.06V10H3.83V4.73H2.36V3H3.76v-1A2.05,2.05,0,0,1,5.8,0H7.64V1.71H6.79a.73.73,0,0,0-.72.72V3H7.64Z"
+            ></path>
+        </svg>
+    </ShareNetwork>
+</template>
+
+<script>
+export default {
+    props: ["shareInfo", "size"],
+};
+</script>
---
+++ resources/js/components/SocialSharing/ShareGroup.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center space-x-5 mt-3">
+        <span class="font-semibold text-black">Share:</span>
+        <div class="flex items-center justify-start space-x-5">
+            <Facebook :shareInfo="shareInfo"/>
+            <Twitter :shareInfo="shareInfo"/>
+            <Pinterest :shareInfo="shareInfo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import Facebook from "./Facebook";
+import Twitter from "./Twitter";
+import Pinterest from "./Pinterest";
+
+export default {
+    //expecting an object for shareInfo with
+    // url, title, description,
+    props: ["shareInfo"],
+    components: {
+        Facebook,
+        Twitter,
+        Pinterest,
+    },
+};
+</script>
