29ce104395198d0289b219088cbbf7a5f5770ca8
TheBoringBot <richardsgareth@icloud.com>
2022-05-14 10:46:22 +0200

    feat: new plugin for social sharing

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,8 +5,30 @@ import {createInertiaApp} from "@inertiajs/inertia-vue3";
 import {InertiaProgress} from "@inertiajs/progress";
 import {createStore} from "vuex";
 import storeData from "./Store/Store";
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import VueSocialSharing from "vue-social-sharing";
 
 const store = new createStore(storeData);
+
+// Options for Toast Notifications
+const toastOptions = {
+    transition: "Vue-Toastification__fade",
+    maxToasts: 5,
+    newestOnTop: true,
+    position: "top-right",
+    timeout: 1990,
+    closeOnClick: true,
+    pauseOnFocusLoss: false,
+    pauseOnHover: false,
+    draggable: true,
+    draggablePercent: 0.3,
+    showCloseButtonOnHover: false,
+    hideProgressBar: true,
+    closeButton: false,
+    icon: true,
+    rtl: false,
+};
 
 const appName =
     window.document.getElementsByTagName("title")[0]?.innerText || "Laravel";
@@ -25,6 +47,8 @@ createInertiaApp({
                     .catch((error) => console.error(error))
             )
             .use(store)
+            .use(VueSocialSharing)
+            .use(Toast, toastOptions)
             .mixin({methods: {route}})
             .mount(el);
     },
