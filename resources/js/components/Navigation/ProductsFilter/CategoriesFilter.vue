98067fcffdbf94c0e441d68d571d07c367acae13
TheBoringBot <richardsgareth@icloud.com>
2022-05-16 17:13:45 +0200

    feat: Filter products via categories intial build

---
+++ resources/js/components/Navigation/ProductsFilter/CategoriesFilter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex items-center space-x-5">
+        <ul class="flex space-x-5">
+            <li @click="$store.commit('sortCategory', 'Port')">All</li>
+            <li
+                    v-for="category in categories"
+                    v-text="category.name"
+                    @click="$store.commit('sortCategory', { category: category })"
+            ></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["categories"],
+
+    methods: {},
+};
+</script>
