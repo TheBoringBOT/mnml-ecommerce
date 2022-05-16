2d64fa8cf3a76ab453c28d2a032e5e619e674616
TheBoringBot <richardsgareth@icloud.com>
2022-05-16 17:10:32 +0200

    feat: product slider initial build

---
+++ resources/js/components/Sliders/ProductSlider.vue
@@ -0,0 +1,96 @@
+<template>
+    <h3
+            v-if="title"
+            v-text="title"
+            class="font-semibold uppercase text-2xl"
+    ></h3>
+    <swiper
+            :slidesPerView="1"
+            :spaceBetween="10"
+            :pagination="{
+            clickable: true,
+        }"
+            :breakpoints="{
+            '@0.00': {
+                slidesPerView: 2,
+                spaceBetween: 10,
+            },
+
+            '@1.00': {
+                slidesPerView: 3,
+                spaceBetween: 40,
+            },
+            '@1.50': {
+                slidesPerView: 4,
+                spaceBetween: 50,
+            },
+        }"
+            :modules="modules"
+            class="mySwiper"
+    >
+        <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide
+    ><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide
+    ><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide
+    ><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide
+    ><swiper-slide>Slide 9</swiper-slide>
+    </swiper>
+</template>
+<script>
+// Import Swiper Vue.js components
+import {Swiper, SwiperSlide} from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/pagination";
+
+// import required modules
+import {Pagination} from "swiper";
+
+export default {
+    props: ["title"],
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    setup() {
+        return {
+            modules: [Pagination],
+        };
+    },
+};
+</script>
+
+<style scoped>
+.swiper {
+    width: 100%;
+    height: 300px;
+}
+
+.swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #ccc;
+
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+}
+
+.swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+</style>
