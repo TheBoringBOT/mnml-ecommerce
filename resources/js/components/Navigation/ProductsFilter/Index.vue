512db4b933672270457d3721db0b924efa405998
TheBoringBot <richardsgareth@icloud.com>
2022-05-16 18:40:02 +0200

    feat: product filters group

---
+++ resources/js/components/Navigation/ProductsFilter/Index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex justify-between items-center pt-10">
+        <!--<CategoriesFilter :categories="categories"/>-->
+
+        <span></span>
+        <SortFilter/>
+    </div>
+</template>
+
+<script>
+import CategoriesFilter from "./CategoriesFilter";
+import SortFilter from "./SortFilter";
+import {useStore} from "vuex";
+
+export default {
+    components: {
+        CategoriesFilter,
+        SortFilter,
+    },
+    computed: {
+        categories() {
+            return useStore().state.categories;
+        },
+    },
+};
+</script>
