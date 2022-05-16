ef2dc33d980c512ddd4ec0a9fc4f15d0783473c6
TheBoringBot <richardsgareth@icloud.com>
2022-05-16 18:41:06 +0200

    style: reduced title padding

---
+++ resources/js/components/Products/Grid/Grid.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="flex flex-col my-16">
+        <div class="flex justify-between items-center">
+            <span
+                    v-if="title"
+                    v-text="title"
+                    class="w-full text-left text-lg pb-5 tracking-wide font-semibold uppercase"
+            ></span>
+            <Link
+                    :href="route('products')"
+                    v-if="showButton"
+                    class="text-brand font-semibold whitespace-nowrap block"
+            >View All</Link
+                >
+        </div>
+        <div class="grid auto-rows-auto gap-8" :class="gridSize">
+            <ProductGridItem v-for="product in products" :product="product"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProductGridItem from "./GridItem";
+import {Link} from "@inertiajs/inertia-vue3";
+
+export default {
+    props: ["products", "gridSize", "title", "showButton"],
+    components: {
+        ProductGridItem,
+        Link,
+    },
+};
+</script>
