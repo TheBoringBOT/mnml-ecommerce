7bffb358364fbed300f143369a102bfd051b197e
TheBoringBot <richardsgareth@icloud.com>
2022-05-17 16:44:20 +0200

    feat: filter products via categories id

--- resources/js/Pages/Frontend/Product/Index.vue
+++ resources/js/Pages/Frontend/Product/Index.vue
@@ -1,99 +1,52 @@
 <template>
     <GuestLayout>
-        <section class="text-gray-700 body-font">
-            <div class="container px-5 py-24 mx-auto">
-                <div
-                    class="grid gap-5 grid-cols-1 md:grid-cols-3 xl:grid-cols-4"
-                    v-if="!products.length"
-                >
-                    <div class="w-full p-4">
-                        <a class="block relative h-48 rounded overflow-hidden">
-                            <img
-                                alt="ecommerce"
-                                class="object-cover object-center w-full h-full block"
-                                src="https://dummyimage.com/420x260"
-                            />
-                        </a>
-                        <div class="mt-4">
-                            <h3
-                                class="text-gray-500 text-xs tracking-widest title-font mb-1 uppercase inline-block mr-2"
-                            >
-                                N/A
-                            </h3>
-                            <h2
-                                class="text-gray-900 title-font text-lg font-medium"
-                            >
-                                Loading
-                            </h2>
-                            <p class="mt-1">$0.00</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="flex flex-wrap -m-4" v-else>
-                    <div
-                        class="lg:w-1/4 md:w-1/2 p-4 w-full mb-4"
-                        v-for="product in products"
-                        :key="product.id"
-                    >
-                        <Link
-                            class="block relative h-48 rounded overflow-hidden"
-                            :href="/product/ + product.slug"
-                        >
-                            <img
-                                alt="ecommerce"
-                                class="object-cover object-center w-full h-full block"
-                                src="https://dummyimage.com/420x260"
-                            />
-                        </Link>
-                        <div class="mt-4">
-                            <h3
-                                class="text-gray-500 text-xs tracking-widest title-font mb-1 uppercase inline-block mr-2"
-                                v-for="category in product.categories"
-                                v-text="category.name"
-                            ></h3>
-                            <h2
-                                class="text-gray-900 title-font text-lg font-medium"
-                                v-text="product.name"
-                            ></h2>
-                            <p
-                                class="mt-1"
-                                v-text="formatCurrency(product.price)"
-                            ></p>
-                            <!--<p v-text="product.location"></p>-->
-                            <span
-                                v-if="product.available <= 0"
-                                class="text-red-600"
-                                >Sold Out</span
-                            >
-                            <span v-else v-text="product.available"></span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+        <ContentSpacerTop/>
+        <ContentWrapper>
+            <ProductsFilter/>
+            <ProductGrid
+                    :products="products"
+                    gridSize="grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
+            />
+        </ContentWrapper>
     </GuestLayout>
 </template>
 <script>
-import GuestLayout from "@/Layouts/Customer";
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import { useStore } from "vuex";
+import GuestLayout from "@/Layouts/GuestLayout";
+import ContentSpacerTop from "@/Layouts/ContentSpacerTop";
+import ContentWrapper from "@/Layouts/ContentWrapper";
+import ProductGrid from "@/components/Products/Grid/Grid";
+import ProductsFilter from "@/Components/Navigation/ProductsFilter/Index";
+import {Head, Link} from "@inertiajs/inertia-vue3";
+import {useStore} from "vuex";
 
 export default {
-    components: { Head, Link, GuestLayout },
-    // props: ["products"],
-    methods: {
-        formatCurrency(amount) {
-            amount = amount / 100;
-            return amount.toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-            });
-        },
+    props: ["categoryId"],
+    components: {
+        Head,
+        Link,
+        GuestLayout,
+        ContentSpacerTop,
+        ContentWrapper,
+        ProductGrid,
+        ProductsFilter,
     },
+
     computed: {
-        products() {
-            console.log(useStore().state);
-            return useStore().state.products;
+        products(props) {
+            // this page is used for categories + shop index.
+            // categoryId is only available on /category/{id} pages
+            const categoryId = props.categoryId
+                ? parseInt(props.categoryId)
+                : null;
+            const products = useStore().state.products;
+            // if no category id return the first 16 products
+            if (categoryId === null) return products.slice(0, 16);
+            // if the category id is available then
+            // filter the products with the category
+            // using lodash filter
+            return _.filter(products, {
+                categories: [{id: categoryId}],
+            });
         },
     },
 };
