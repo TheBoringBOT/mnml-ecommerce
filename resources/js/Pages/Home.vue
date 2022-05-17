56bc03bf91c96d885cc19adeb3c1f63bc43c53a6
TheBoringBot <richardsgareth@icloud.com>
2022-05-17 17:15:12 +0200

    style: spacing Y added around grid

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,14 +1,39 @@
 <template>
-    <GuestLayout> </GuestLayout>
+    <HeroSlider/>
+
+    <GuestLayout>
+        <ContentWrapper class="my-16">
+            <ProductGrid
+                    yMargin="true"
+                    showButton="true"
+                    title="Some of our Favorites"
+                    :products="products"
+                    gridSize="grid-cols-1 md:grid-cols-2
+            lg:grid-cols-4"
+            /></ContentWrapper>
+    </GuestLayout>
 </template>
 <script>
-import GuestLayout from "@/Layouts/Customer";
-import { Head, Link } from "@inertiajs/inertia-vue3";
-import { useStore } from "vuex";
+import GuestLayout from "@/Layouts/GuestLayout";
+import ContentWrapper from "@/Layouts/ContentWrapper";
+import {Head, Link} from "@inertiajs/inertia-vue3";
+import HeroSlider from "@/components/Sliders/HeroSlider";
+import ProductGrid from "@/components/Products/Grid/Grid";
+
+import {useStore} from "vuex";
+import ProductSlider from "@/components/Sliders/ProductSlider";
 
 export default {
-    components: { Head, Link, GuestLayout },
-    // props: ["products"],
+    components: {
+        ProductSlider,
+        Head,
+        Link,
+        GuestLayout,
+        ContentWrapper,
+        HeroSlider,
+        ProductGrid,
+    },
+    props: ["productSlug", "categories"],
     methods: {
         formatCurrency(amount) {
             amount = amount / 100;
@@ -20,8 +45,14 @@ export default {
     },
     computed: {
         products() {
-            console.log(useStore().state);
-            return useStore().state.products;
+            const shuffledProducts = _.shuffle(useStore().state.products);
+            return shuffledProducts.slice(0, 8);
+        },
+
+        product(props) {
+            return this.products.find(
+                (product) => product.slug === props.productSlug
+            );
         },
     },
 };
