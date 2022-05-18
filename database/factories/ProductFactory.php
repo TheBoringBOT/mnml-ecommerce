f0889fefe4409d9f6821d6dc1eee46bfe508a709
TheBoringBot <richardsgareth@icloud.com>
2022-05-18 21:55:08 +0200

    feat: more dummy data columns

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,12 +19,13 @@ class ProductFactory extends Factory {
 
 		return [
 			'name'        => $name,
-			'brand'       => $this->faker->name,
+			'story'       => $this->faker->text,
+			'care'        => $this->faker->text,
 			'slug'        => Str::slug( $name ),
 			'description' => $this->faker->realText( 320 ),
 			'price'       => $this->faker->numberBetween( 1000, 10000 ),
-			'image_url'   => 'https://i.pravatar.cc/1000',
-			'available'   => $this->faker->numberBetween( 5, 80 ),
+
+			'available' => $this->faker->numberBetween( 5, 80 ),
 
 		];
 	}
