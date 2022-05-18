d544ba80f53bd8d3164da9484ceb4255b1aaab7c
TheBoringBot <richardsgareth@icloud.com>
2022-05-18 18:26:02 +0200

    build: intervention image package added

--- config/app.php
+++ config/app.php
@@ -167,6 +167,8 @@ return [
 		 * Package Service Providers...
 		 */
 		Spatie\Permission\PermissionServiceProvider::class,
+		Intervention\Image\ImageServiceProvider::class,
+
 
 		/*
 		 * Application Service Providers...
@@ -233,6 +235,8 @@ return [
 		'URL'          => Illuminate\Support\Facades\URL::class,
 		'Validator'    => Illuminate\Support\Facades\Validator::class,
 		'View'         => Illuminate\Support\Facades\View::class,
+		'Image'        => Intervention\Image\Facades\Image::class
+
 
 	],
 
