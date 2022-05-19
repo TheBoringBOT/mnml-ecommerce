cc2d8d9244d3cd3f7e028e8f769e5d6f5763ff6c
TheBoringBot <richardsgareth@icloud.com>
2022-05-19 15:14:56 +0200

    feat: check if images are available before adding

--- resources/js/components/Products/Grid/GridItem.vue
+++ resources/js/components/Products/Grid/GridItem.vue
@@ -1,24 +1,27 @@
 <template>
-    <Link :href="/product/ + product.slug" class="flex flex-col space-y-5">
+    <Link :href="`/product/${product.slug}`" class="flex flex-col space-y-5">
         <figure class="flex relative pt-[100%] overflow-hidden">
             <img
-                    class="grow absolute top-0 left-0 h-full w-full"
-                    :src="getMDImage(product.image)"
-                    :alt="product.title"
+                class="grow absolute top-0 left-0 h-full w-full"
+                :src="`${
+                    product.images.length &&
+                    getMDImage(product.images[0].image_url)
+                }`"
+                :alt="product.name"
             />
         </figure>
         <div class="flex justify-between items-center">
             <span class="font-semibold uppercase" v-text="product.name"></span>
             <span
-                    class="text-grey"
-                    v-text="formatCurrency(product.price)"
+                class="text-grey"
+                v-text="formatCurrency(product.price)"
             ></span>
         </div>
     </Link>
 </template>
 
 <script>
-import {Link} from "@inertiajs/inertia-vue3";
+import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
     props: ["product"],
