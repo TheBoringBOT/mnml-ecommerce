d640ff028e0af56866395fe746e46121cf75dec8
TheBoringBot <richardsgareth@icloud.com>
2022-05-19 11:04:50 +0200

    feat: urls configured with controller

--- resources/js/components/Backend/CategoriesList.vue
+++ resources/js/components/Backend/CategoriesList.vue
@@ -1,6 +1,21 @@
-2<template>
+<template>
+    <h2 class="text-3xl text-gray-500 mb-5">Categories</h2>
     <div class="flex items-center flex-wrap w-full space-x-5">
         <Link
+            :href="'/dashboard/products'"
+            :class="{
+                'text-black bg-gray-300': $page.url === '/dashboard/products',
+            }"
+            class="rounded bg-black text-white px-2"
+            >All</Link
+        >
+
+        <Link
+            :href="`/dashboard/products/category/${category.id}`"
+            :class="{
+                'text-black bg-gray-300':
+                    $page.url === '/dashboard/products/category/' + category.id,
+            }"
             class="rounded bg-black text-white px-2"
             v-for="category in categories"
             v-text="category.name"
