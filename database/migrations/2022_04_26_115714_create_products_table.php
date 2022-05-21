bc84fd2d58a53cb7004876b0c160cfbbc2f4ca37
TheBoringBot <richardsgareth@icloud.com>
2022-05-21 12:10:05 +0200

    feat: new column for product excerpt description

--- database/migrations/2022_04_26_115714_create_products_table.php
+++ database/migrations/2022_04_26_115714_create_products_table.php
@@ -18,16 +18,13 @@ class CreateProductsTable extends Migration {
 
 		Schema::create( 'products', function ( Blueprint $table ) {
 
-			// default text for demo app
-			$loremIpsum = " Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis. Maecenas ut tellus mi. Proin tincidunt, lectus eu volutpat mattis, ante metus lacinia tellus, vitae condimentum nulla enim bibendum nibh. Praesent turpis risus, interdum nec venenatis id, pretium sit amet purus. Interdum et malesuada fames.";
-
-			$care = "Hand wash only";
 
 			$table->id();
 			$table->string( 'name' );
 			$table->string( 'slug' )->unique();
 			$table->string( 'sku' )->unique();
 			$table->text( 'description' );
+			$table->string( 'excerpt', 150 );
 			$table->text( 'story' );
 			$table->string( 'care' )->nullable();
 			$table->string( 'materials' )->nullable();
