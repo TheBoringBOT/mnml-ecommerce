77a9a92cefab8c6a3b51c0f18b50630a912280be
TheBoringBot <richardsgareth@icloud.com>
2022-05-21 19:16:23 +0200

    style: small changes

--- resources/js/components/NavLink.vue
+++ resources/js/components/NavLink.vue
@@ -1,18 +1,18 @@
 <script setup>
-import { computed } from "vue";
-import { Link } from "@inertiajs/inertia-vue3";
+import {computed} from "vue";
+import {Link} from "@inertiajs/inertia-vue3";
 
 const props = defineProps(["href", "active"]);
 
 const classes = computed(() =>
     props.active
-        ? "inline-flex items-center px-1 pt-1  text-sm font-bold leading-5 text-brand focus:outline-none  transition  duration-150 ease-in-out"
-        : "inline-flex items-center px-1 pt-1  text-sm font-medium leading-5 text-black-2 hover:text-black  focus:outline-none focus:text-black focus:border-black transition duration-150 ease-in-out"
+        ? "inline-flex items-center px-1 pt-1  text-sm text-black font-medium leading-5  focus:outline-none  transition  duration-150 ease-in-out uppercase"
+        : "inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-grey hover:text-black  focus:outline-none focus:text-black focus:border-black transition duration-150 ease-in-out uppercase"
 );
 </script>
 
 <template>
     <Link :href="href" :class="classes">
-        <slot />
+        <slot/>
     </Link>
 </template>
