1c448d246c19a7753bae033ce18a5b05e440b076
TheBoringBot <richardsgareth@icloud.com>
2022-05-21 19:15:46 +0200

    style: small color changes

--- resources/js/components/Products/Grid/Grid.vue
+++ resources/js/components/Products/Grid/Grid.vue
@@ -2,26 +2,26 @@
     <div class="flex flex-col">
         <div class="flex justify-between items-center pb-5">
             <span
-                    v-if="title"
-                    v-text="title"
-                    class="w-full text-left text-lg tracking-wide font-semibold uppercase"
+                v-if="title"
+                v-text="title"
+                class="w-full text-left text-lg tracking-wide font-semibold uppercase"
             ></span>
             <Link
-                    :href="route('products')"
-                    v-if="showButton"
-                    class="text-brand hover:text-brand-hover font-semibold whitespace-nowrap block uppercase"
-            >View All</Link
-                >
+                :href="route('products')"
+                v-if="showButton"
+                class="text-grey hover:text-brand whitespace-nowrap block uppercase"
+                >View All</Link
+            >
         </div>
         <div class="grid auto-rows-auto gap-8" :class="gridSize">
-            <ProductGridItem v-for="product in products" :product="product"/>
+            <ProductGridItem v-for="product in products" :product="product" />
         </div>
     </div>
 </template>
 
 <script>
 import ProductGridItem from "./GridItem";
-import {Link} from "@inertiajs/inertia-vue3";
+import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
     props: ["products", "gridSize", "title", "showButton"],
