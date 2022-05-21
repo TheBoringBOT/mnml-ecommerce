b714936722831dad2af1a9fa2e45a4a0a7d7e42b
TheBoringBot <richardsgareth@icloud.com>
2022-05-21 18:54:25 +0200

    style: changed skeleton background color

--- resources/js/components/Tabs/Skeleton/TabsWrapperSkeleton.vue
+++ resources/js/components/Tabs/Skeleton/TabsWrapperSkeleton.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="tabs w-full bg-gray-100 animate-pulse mx-auto mt-10 h-64">
-        <div class="bg-white w-full py-5 hidden lg:block">
+    <section class="tabs w-full bg-skeleton animate-pulse mx-auto mt-10 h-64">
+        <div class="bg-off-white w-full py-5 hidden lg:block">
             <ContentWrapper>
                 <ul
                         class="flex flex-wrap items-start space-y-5 md:space-y-0 md:space-x-5 lg:items-center max-w-7xl"
                 >
-                    <li class="bg-gray-100 h-8 w-48"></li>
-                    <li class="bg-gray-100 h-8 w-48"></li>
+                    <li class="bg-skeleton h-8 w-48"></li>
+                    <li class="bg-skeleton h-8 w-48"></li>
                 </ul>
             </ContentWrapper>
         </div>
