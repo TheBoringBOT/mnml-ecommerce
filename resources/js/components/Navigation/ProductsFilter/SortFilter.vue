67990b324075143f0c96c79e792f2f4a8ca0e61f
TheBoringBot <richardsgareth@icloud.com>
2022-05-23 18:36:01 +0200

    feat: current sort title

--- resources/js/components/Navigation/ProductsFilter/SortFilter.vue
+++ resources/js/components/Navigation/ProductsFilter/SortFilter.vue
@@ -3,50 +3,50 @@
     <Menu as="div" class="relative inline-block text-left z-10">
         <div>
             <MenuButton
-                    class="flex items-center justify-between w-[200px] w-full rounded-sm border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
+                class="flex items-center justify-between w-[170px] w-full border-0 px-4 py-2 text-sm font-medium border-b border-black"
             >
-                <span class="block text-sm">Sort By:</span>
+                <span class="block text-sm">Sort:</span>
                 <span v-text="currentSort"></span>
             </MenuButton>
         </div>
 
         <transition
-                enter-active-class="transition ease-out duration-100"
-                enter-from-class="transform opacity-0 scale-95"
-                enter-to-class="transform opacity-100 scale-100"
-                leave-active-class="transition ease-in duration-75"
-                leave-from-class="transform opacity-100 scale-100"
-                leave-to-class="transform opacity-0 scale-95"
+            enter-active-class="transition ease-out duration-100"
+            enter-from-class="transform opacity-0 scale-95"
+            enter-to-class="transform opacity-100 scale-100"
+            leave-active-class="transition ease-in duration-75"
+            leave-from-class="transform opacity-100 scale-100"
+            leave-to-class="transform opacity-0 scale-95"
         >
             <MenuItems
-                    class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
             >
                 <div class="py-1">
                     <MenuItem v-slot="{ active }">
                         <span
-                                @click="
+                            @click="
                                 $store.commit('sortProducts', ['price', 'asc'])
                             "
-                                class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
-                        >Price - Low to high</span
+                            class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
+                            >Price - Low to high</span
                         >
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
                         <span
-                                @click="
+                            @click="
                                 $store.commit('sortProducts', ['price', 'desc'])
                             "
-                                class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
-                        >Price - High to Low</span
+                            class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
+                            >Price - High to Low</span
                         >
                     </MenuItem>
                     <MenuItem v-slot="{ active }">
                         <span
-                                @click="
+                            @click="
                                 $store.commit('sortProducts', ['id', 'desc'])
                             "
-                                class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
-                        >New Items</span
+                            class="hover:bg-gray-100 cursor-pointer block px-4 py-2 text-sm"
+                            >New Items</span
                         >
                     </MenuItem>
                 </div>
@@ -56,8 +56,8 @@
 </template>
 
 <script>
-import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
-import {useStore} from "vuex";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+import { useStore } from "vuex";
 
 export default {
     components: {
@@ -69,13 +69,20 @@ export default {
     computed: {
         currentSort() {
             const current = useStore().state.sort;
+
+            console.table(current);
             let text = "";
 
-            current.order === null
-                ? (text = current.sort)
-                : current.order === "asc"
-                ? (text = current.sort + " " + "Up")
-                : current.sort + " " + "Down";
+            switch (current.order) {
+                case "asc":
+                    text = "Price Up";
+                    break;
+                case "desc":
+                    text = current.sort === "id" ? "New Items" : "Price Down";
+                    break;
+                default:
+                    text = "Default";
+            }
             return text;
         },
     },
