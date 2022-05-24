3bc3572e16d55099d5996d1059ee615dc53298e7
TheBoringBot <richardsgareth@icloud.com>
2022-05-24 14:26:44 +0200

    style:small fixes

--- resources/js/components/Checkout/Items.vue
+++ resources/js/components/Checkout/Items.vue
@@ -1,29 +1,29 @@
 <template>
     <div class="py-8 lg:py-0">
         <div
-            class="grid grid-cols-6 mb-5"
-            v-for="(item, index) in cart"
-            :key="item.id"
+                class="grid grid-cols-6 mb-5"
+                v-for="(item, index) in cart"
+                :key="item.id"
         >
             <!-- item image -->
             <figure class="rounded col-span-1">
                 <img
-                    class="object-cover w-16 h-16 rounded shadow-sm border border-gray-200"
-                    src="https://i.pravatar.cc/100"
-                    :alt="item.name"
+                        class="object-cover w-16 h-16 rounded shadow-sm border border-gray-200"
+                        src="https://i.pravatar.cc/100"
+                        :alt="item.name"
                 />
             </figure>
             <!-- item name + quantity -->
             <div
-                class="col-span-5 flex-col items-start justify-center text-sm my-auto"
+                    class="col-span-5 flex-col items-start justify-center text-sm my-auto"
             >
                 <div
-                    class="flex flex-col items-start justify-center space-y-2 truncate mx-2"
+                        class="flex flex-col items-start justify-center space-y-2 truncate mx-2"
                 >
                     <div class="flex justify-between items-center w-full">
                         <span
-                            class="max-w-[70%] truncate whitespace-nowrap font-semibold"
-                            v-text="item.name"
+                                class="max-w-[70%] truncate whitespace-nowrap font-medium"
+                                v-text="item.name"
                         >
                         </span>
                         <!-- item price -->
@@ -33,13 +33,13 @@
                     <div class="flex space-x-5 justify-center">
                         <div class="flex items-center space-x-5">
                             <span
-                                class="text-gray-700 text-xs"
-                                v-text="`Qty: ${item.quantity}`"
+                                    class="text-gray-700 text-xs"
+                                    v-text="`Qty: ${item.quantity}`"
                             ></span>
                             <div class="flex items-center space-x-1 text-black">
                                 <button
-                                    class="p-2 -m-2"
-                                    @click="
+                                        class="p-2 -m-2"
+                                        @click="
                                         $store.commit(
                                             'decreaseQuantity',
                                             index,
@@ -48,24 +48,24 @@
                                     "
                                 >
                                     <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="14"
-                                        height="14"
-                                        fill="currentColor"
-                                        viewBox="0 0 16 16"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="14"
+                                            height="14"
+                                            fill="currentColor"
+                                            viewBox="0 0 16 16"
                                     >
                                         <path
-                                            d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"
+                                                d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z"
                                         />
                                     </svg>
                                 </button>
                                 <button
-                                    :class="[
+                                        :class="[
                                         item.available === item.quantity &&
                                             'pointer-events-none opacity-50',
                                     ]"
-                                    class="p-2 -m-2"
-                                    @click="
+                                        class="p-2 -m-2"
+                                        @click="
                                         $store.commit(
                                             'increaseQuantity',
                                             index,
@@ -74,14 +74,14 @@
                                     "
                                 >
                                     <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="14"
-                                        height="14"
-                                        fill="currentColor"
-                                        viewBox="0 0 16 16"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            width="14"
+                                            height="14"
+                                            fill="currentColor"
+                                            viewBox="0 0 16 16"
                                     >
                                         <path
-                                            d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"
+                                                d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z"
                                         />
                                     </svg>
                                 </button>
