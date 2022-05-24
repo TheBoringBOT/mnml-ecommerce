66c511523ed015fb2415dfff846db62ef221048f
TheBoringBot <richardsgareth@icloud.com>
2022-05-24 14:23:16 +0200

    style: mobile cleaned up

--- resources/js/Pages/Frontend/Order/Checkout.vue
+++ resources/js/Pages/Frontend/Order/Checkout.vue
@@ -1,46 +1,81 @@
 <template>
-    <!-- TODO: clean up this file into seperate components -->
     <Head>
         <title>Checkout</title>
         <meta name="description" content="Checkout at Sirène"/>
     </Head>
+    <!-- demo notification -->
+    <div
+            :class="showNotice ? 'block' : 'hidden'"
+            class="fixed z-50 bottom-0 right-0 lg:bottom-5 lg:right-5 bg-red-500 text-sm font-medium text-white text-center lg:text-left text-medium w-full lg:max-w-[400px]"
+    >
+        <div class="p-5">
+            <span
+                    @click="closeNotice"
+                    class="cursor-pointer z-10 absolute top-3 right-3 opacity-50 hover:opacity-100"
+            >
+                X
+            </span>
+
+            <p class="font-semibold">Test payment with:</p>
+            <div class="flex flex-col space-y-1 mt-2">
+                <p>Number: 4242424242424242</p>
+                <p>CVC: Any 3 digits</p>
+                <p>Date: Any future date</p>
+            </div>
+        </div>
+    </div>
     <div class="w-full mx-auto bg-white">
         <div
+                class="flex justify-between items-center bg-white h-16 lg:hidden w-full px-5"
+        >
+            <BackButton class="relative m-0 mx-0 p-0"/>
+
+            <h1 class="text-md font-medium">Checkout</h1>
+        </div>
+        <div
                 v-if="cart.length"
-                class="max-w-[600px] mx-auto lg:max-w-full w-full grid grid-cols-1 lg:grid-cols-4 min-h-screen"
+                class="mx-auto w-full grid grid-cols-1 lg:grid-cols-4 min-h-screen"
         >
             <!-- customer details / payment details-->
             <div
-                    class="lg:col-span-2 px-10 relative row-start-2 lg:row-start-1"
+                    class="lg:col-span-2 px-5 lg:px-10 relative row-start-2 lg:row-start-1"
             >
-                <ContentSpacerTop/>
-                <div class="max-w-[500px] flex flex-col ml-auto mr-8">
-                    <BackButton/>
-                    <div class="hidden lg:block pb-5">
-                        <!--<ApplicationLogo />-->
-                        <h1 class="text-lg font-semibold">Checkout</h1>
+                <div class="max-w-[500px] mx-auto lg:max-w-full">
+                    <div
+                            class="max-w-[500px] flex flex-col ml-auto lg:mr-8 pt-8 lg:pt-0"
+                    >
+                        <div class="hidden lg:flex">
+                            <ContentSpacerTop/>
+                            <BackButton/>
+                        </div>
+                        <div class="hidden lg:block pb-5">
+                            <!--<ApplicationLogo />-->
+                            <h1 class="text-lg font-semibold">Checkout</h1>
+                        </div>
+                        <!--<h3 class="text-2xl font-semibold text-black">Payment Info</h3>-->
+                        <CheckoutForm :cartTotal="cartTotal"/>
+                        <hr class="w-full bg-gray-200 mt-5 mb-3"/>
+                        <CheckoutFooter/>
                     </div>
-                    <!--<h3 class="text-2xl font-semibold text-black">Payment Info</h3>-->
-                    <CheckoutForm :cartTotal="cartTotal"/>
-                    <hr class="w-full bg-gray-200 mt-5 mb-3"/>
-                    <CheckoutFooter/>
                 </div>
             </div>
 
             <!-- checkout items + total -->
             <div
-                    class="lg:bg-neutral-50 border-b lg:border-l border-gray-200 w-full mx-auto overflow-auto lg:col-span-2 px-10 text-gray-900 font-medium h-fit lg:h-full"
+                    class="bg-neutral-50 border-b border-t lg:border-l border-gray-200 w-full mx-auto overflow-auto lg:col-span-2 px-5 lg:px-10 text-gray-900 font-medium h-fit lg:h-full"
             >
-                <ContentSpacerTop/>
-                <div class="lg:max-w-[400px] flex lg:mr-auto lg:ml-8">
-                    <div class="flex flex-col w-full">
-                        <CheckoutItems
-                                :cart="cart"
-                                :cartLineTotal="cartLineTotal"
-                        />
+                <div class="max-w-[500px] mx-auto lg:max-w-full">
+                    <ContentSpacerTop class="hidden lg:block"/>
+                    <div class="lg:max-w-[400px] flex lg:mr-auto lg:ml-8">
+                        <div class="flex flex-col w-full">
+                            <CheckoutItems
+                                    :cart="cart"
+                                    :cartLineTotal="cartLineTotal"
+                            />
 
-                        <!-- totals + shipping -->
-                        <CheckoutTotal :cartTotal="cartTotal"/>
+                            <!-- totals + shipping -->
+                            <CheckoutTotal :cartTotal="cartTotal"/>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -78,8 +113,16 @@ export default {
         BackButton,
         EmptyCartNotification,
     },
+    data() {
+        return {
+            showNotice: true,
+        };
+    },
 
     methods: {
+        closeNotice() {
+            this.showNotice = !this.showNotice;
+        },
         // return cart amount
         cartLineTotal(item) {
             let amount = item.price * item.quantity;
