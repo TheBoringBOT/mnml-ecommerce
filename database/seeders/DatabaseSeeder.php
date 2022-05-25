e4a9cd9f051908df6cdd3ba9a90232d2e6536fd7
TheBoringBot <richardsgareth@icloud.com>
2022-05-25 12:45:04 +0200

    refactor: commented out product seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,15 @@ class DatabaseSeeder extends Seeder {
 	public function run() {
 
 		// run the factories
-		Product::factory()->count( 20 )->create();
+		//		Product::factory()->count( 20 )->create();
 		Category::factory()->count( 5 )->create();
 
 
-		// connecting all the categories to the product
-		$categories = Category::all();
-		Product::all()->each( function ( $product ) use ( $categories ) {
-			$product->categories()->attach( $categories->random( 2 )->pluck( 'id' )->toArray() );
-		} );
+		//		// connecting all the categories to the product
+		//		$categories = Category::all();
+		//		Product::all()->each( function ( $product ) use ( $categories ) {
+		//			$product->categories()->attach( $categories->random( 2 )->pluck( 'id' )->toArray() );
+		//		} );
 
 	}
 }
